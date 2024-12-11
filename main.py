c8ab6ece859cd97308761529041bbb8e5e6bb059
arseeenyyy <rubtsov113@mail.ru>
2024-12-11 13:40:13 +0300

    draw a simple circle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import pymunk
+import pymunk.pygame_util
 
 pygame.init() 
 
@@ -9,11 +11,27 @@ SCREEN_HEIGHT = 678
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Billiards")
 
+space = pymunk.Space()
+draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+def create_ball(radius, position): 
+    body = pymunk.Body()
+    body.position = position
+    shape = pymunk.Circle(body, radius)
+    shape.mass = 5 #TODO create a opportunity to change mass
+
+    space.add(body, shape)
+    return shape
+
+new_ball = create_ball(25, (300, 100))
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    space.debug_draw(draw_options)
+    pygame.display.update()
 
 
 pygame.quit()
